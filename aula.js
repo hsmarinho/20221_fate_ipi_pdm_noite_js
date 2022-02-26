4000aea211c132ff382513fafe3c785950a2ed6c
Henrique Marinho <henrmarinho@gmail.com>
2022-02-25 21:32:01 -0300

    Retomar  da secao 1.5 da apostila 02 falando sobre vetore

---
+++ aula.js
@@ -0,0 +1,53 @@
+
+
+//==
+//===
+
+
+// const a = []
+// const b = a
+// console.log(a == b)
+
+// console.log([] == [])
+// console.log([] == false)
+
+// console.log(null == undefined)
+// console.log(null == null)
+// console.log(1 == [1])
+// console.log(true == 1)
+// console.log(1 == 1)
+// console.log(1 == "1") //coerção implissita - resultado true
+// console.log(1 ==="1") // === faz a comparação por tipo de dados antes de comparar os valores.
+
+
+
+// const n1 = 2
+// const n2 = '3'
+
+// const n3 = n1 + n2
+// console.log(n3)
+
+// const n4 = n1 Number(n2)
+// console.log(n4)
+
+// var idade =18
+// console.log("oi, " + nome)
+// if (idade >= 18){
+//     //var utiliza içamento "hoist" (o escopo da variavel não é restrito ao bloco)
+//     var nome = "João"
+//     // console.log("Parabéns, " + nome +"você pode dirigir")
+//     console.log(`Parabéns ${nome}. Voce pode dirigir`)
+// }
+
+// console.log("Até mais, " + nome)
+
+// //começando...
+// const nome = "José" //declaração de constante
+// const idade  = 27
+
+// let a = 2
+// let b = "abc"
+// let endereco = `rua k, 12` //declarar string con crase pode ter quebra de linha
+
+// var c = 2+3
+// var d = "abcd"
